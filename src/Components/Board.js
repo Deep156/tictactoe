4ee1eae50e50cc6f11fa82a1a5f3dcf08d35faa4
Deep156 <chauhandeepanjali30@gmail.com>
2021-06-21 20:26:54 +0530

    component square and board created

---
+++ src/Components/Board.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import Square from './Square';
+
+const Board = () => {
+  return (
+    <div>
+      <div>
+        <Square value={0} />
+        <Square value={1} />
+        <Square value={2} />
+      </div>
+
+      <div>
+        <Square value={3} />
+        <Square value={1} />
+        <Square value={1} />
+      </div>
+
+      <div>
+        <Square value={1} />
+        <Square value={1} />
+        <Square value={1} />
+      </div>
+    </div>
+  );
+};
+
+export default Board;
